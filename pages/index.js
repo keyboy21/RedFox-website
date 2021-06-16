3cda5fd19db635822dc03c9c75d85621d46191d8
yunus563 <yunussaydullayev@gmail.com>
2021-06-16 15:39:34 +0300

    made home page

--- pages/index.js
+++ pages/index.js
@@ -11,22 +11,21 @@ export default function Home() {
                 Bizga ishonch bildirgan kompaniyalar <span>250+</span>{" "}
               </h2>
               <p>
-                Biznesda ham hayotda ham eng muhim omil bu ishonchdir. <br/> Bizga
-                ishonch bildirgan kompaniyalar qatorida sizni ham ko'rishdan
-                mamnunmiz.
+                Biznesda ham hayotda ham eng muhim omil bu ishonchdir. <br />{" "}
+                Bizga ishonch bildirgan kompaniyalar qatorida sizni ham
+                ko'rishdan mamnunmiz.
               </p>
             </div>
             <div className="header_bottom">
               <div className="text_line">
                 <span>Enterprise 30+</span>
               </div>
-  
             </div>
           </div>
         </div>
       </header>
       <header>
-      <div className="container">
+        <div className="container">
           <div className="row">
             <div className="header_top">
               <h2>
@@ -47,8 +46,434 @@ export default function Home() {
         </div>
       </header>
       <section>
-        
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-4">
+              <div className="col-lg-12">
+                <img src="/Rectangle 62Najot_talim.jpg" alt="najot" />
+                <p>Najot ta’lim o‘quv markazi</p>
+              </div>
+              <div className="col-lg-12">
+                <img src="/Rectangle 63Derri.jpg" alt="derri" />
+                <p>Derzi kiyim fabrikasi</p>
+              </div>
+            </div>
+            <div className="col-lg-8">
+              <img
+                src="/image 21Nurafshon.jpg"
+                alt="nurafshon"
+                height="93.8%"
+              />
+              <p>Nurafshon smart city</p>
+            </div>
+          </div>
+          <div className="row bottom">
+            <div className="col-lg-6">
+              <img src="/image 22Normal_Nurafshon.jpg" alt="normal_Nur" />
+              <p>Eden saryog‘lari qadoq dizayn</p>
+            </div>
+            <div className="col-lg-6">
+              <img src="/Rectangle 79MIndo.jpg" alt="Mindo" />
+              <p>Mindo mahsuloti uchun qadoq dizayn</p>
+            </div>
+            <div className="col-lg-6">
+              <img src="/Rectangle 389blue_BG.png" alt="normal_Nur" />
+              <p>Supercorn qadoq dizayni</p>
+            </div>
+            <div className="col-lg-6">
+              <img src="/Rectangle 390Ping_Bg.png" alt="Mindo" />
+              <p>Roma pizza</p>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-lg-4">
+              <img src="/Rectangle 67background.png" alt="redfox" />
+              <p>Vast travel sayohat agentligi </p>
+            </div>
+            <div className="col-lg-4">
+              <img src="/Rectangle 62Blue_b ackground.png" alt="redfox" />
+              <p>Giym internet magazin</p>
+            </div>
+            <div className="col-lg-4">
+              <img src="/Rectangle 67background.png" alt="redfox" />
+              <p>Nurafshon smart city</p>
+            </div>
+            <div className="col-lg-4">
+              <img src="/Rectangle 67background.png" alt="redfox" />
+              <p>Venox</p>
+            </div>
+            <div className="col-lg-4">
+              <img src="/Rectangle 62Blue_b ackground.png" alt="redfox" />
+              <p>Boxcom</p>
+            </div>
+            <div className="col-lg-4">
+              <img src="/Rectangle 67background.png" alt="redfox" />
+              <p>Nurafshon smart city</p>
+            </div>
+          </div>
+        </div>
       </section>
+      <header>
+        <div className="container">
+          <div className="row">
+            <div className="header_top col-lg-7">
+              <h2>
+                Mahalliy bozordagi tajribamiz <span>4 yil</span>
+              </h2>
+              <p>
+                RedFox dizayn studiyasi bir vaqtda ko‘plab kompaniyalar bilan
+                ishlaydigan tajribali katta jamoadir. Mijozlarimiz qatorida
+                sizni ham ko’rishdan mamnunmiz.
+              </p>
+            </div>
+            <div className="header_bottom">
+              <div className="text_line">
+                <span>XIZMATLARIMIZ</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </header>
+      <header>
+        <div className="container">
+          <div className="row">
+            <div className="header_top col-lg-8">
+              <h2>
+                Jamoamizda ekspert va mutaxassislar <span>40+</span>
+              </h2>
+              <p>
+                RedFox katta jamoadan tashkil topgan dizayn studiya. Shuningdek,
+                jamoamizda Frilansus.com baholash tizimida Top 10 kuchli
+                dizaynerlar qatoriga kiruvchi 5 va 4 yulduzli dizaynerlar ham
+                bor.
+              </p>
+            </div>
+            <div className="header_bottom">
+              <div className="text_line">
+                <span>Jamoa a‘zolari</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </header>
+      <div className="experts">
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-4">
+              <div>
+                <img src="/My-User-Pic-001 1PolatovM.jpg" />
+              </div>
+              <div>
+                <h4>Polatov Mavlonbek</h4>
+                <p>UX/UI dizayner · Tajriba: 4 yil</p>
+              </div>
+            </div>
+            <div className="col-lg-4">
+              <div>
+                <img src="/My-User-Pic-001 1AzamM.jpg" />
+              </div>
+              <div>
+                <h4>Azam Muhammadiyev</h4>
+                <p>Grafik dizayner · Tajriba: 10 yil</p>
+              </div>
+            </div>
+            <div className="col-lg-4">
+              <div>
+                <img src="/My-User-Pic-001 1RahmatulloJ.jpg" />
+              </div>
+              <div>
+                <h4>Rahmatullo Jabborov</h4>
+                <p>UX/UI dizayner · Tajriba: 4 yil</p>
+              </div>
+            </div>
+            <div className="col-lg-4">
+              <div>
+                <img src="/My-User-Pic-001 1RahimjonG.jpg" />
+              </div>
+              <div>
+                <h4>Rahimjon Gaziyev</h4>
+                <p>UX/UI dizayner · Tajriba: 4 yil</p>
+              </div>
+            </div>
+            <div className="col-lg-4">
+              <div>
+                <img src="/PicNurmatovO.png" />
+              </div>
+              <div>
+                <h4>Nurmatov Odiljon</h4>
+                <p>UX/UI dizayner · Tajriba: 4 yil</p>
+              </div>
+            </div>
+            <div className="col-lg-4">
+              <div>
+                <img src="/My-User-Pic-001 1BaxtiyorjonG.jpg" />
+              </div>
+              <div>
+                <h4>Baxtiyorjon Gaziyev</h4>
+                <p>UX/UI dizayner · Tajriba: 4 yil</p>
+              </div>
+            </div>
+            <div className="col-lg-4">
+              <div>
+                <img src="/My-User-Pic-001 1SarvgartbekA.jpg" />
+              </div>
+              <div>
+                <h4>Sarvarbek Abdupattoyev</h4>
+                <p>UX/UI dizayner · Tajriba: 4 yil</p>
+              </div>
+            </div>
+            <div className="col-lg-4">
+              <div>
+                <img src="/My-User-Pic-001 1ShokhruhR.jpg" />
+              </div>
+              <div>
+                <h4>Shokhrukh Rakhmatov</h4>
+                <p>UX/UI dizayner · Tajriba: 4 yil</p>
+              </div>
+            </div>
+            <div className="col-lg-4">
+              <div>
+                <img src="/My-User-Pic-001 1UtkirT.jpg" />
+              </div>
+              <div>
+                <h4>Shokhrukh Rakhmatov</h4>
+                <p>UX/UI dizayner · Tajriba: 4 yil</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <header>
+        <div className="container">
+          <div className="row">
+            <div className="header_top col-lg-8">
+              <h2>
+                Mijozlarni qo‘llab quvvatlash, support <span>24/7</span>
+              </h2>
+              <p>
+                RedFox dizayn studiyasi bir vaqtda ko’plab kompaniyalar bilan
+                ishlaydigan tajribali katta jamoadir. Mijozlarimiz qatorida
+                sizni ham ko’rishdan mamnunmiz.
+              </p>
+            </div>
+            <div className="header_bottom">
+              <div className="text_line">
+                <span>Mijozlar fikri</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </header>
+      <div className="reason">
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-4">
+              <div className="bg_item">
+                <div>
+                  <img src="/Frame 8Baxodir_AK.png" />
+                </div>
+                <h5>Bahodir Akbarov</h5>
+                <span>Venox kompaniyasi asoschisi</span>
+                <p>
+                  Bolalar baraka topila qo‘l ko‘zilar dard ko‘rmasin, bosib
+                  chiqarishdan chiqqanidan keyin ko‘rib ko‘z to‘ymayabdi juda
+                  chiroyli chiqdi mosholloh, gapyo‘q hammanglarga rahmat katta
+                </p>
+                <div>
+                  <img src="/GroupVenox.png" />
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-4">
+              <div className="bg_item">
+                <div>
+                  <img src="/Frame 8RixsiboyU.png" />
+                </div>
+                <h5>Rixsiboy Umarov</h5>
+                <span>ITGO asoschisi</span>
+                <p>
+                  Ishlar bilan bo‘lib sizlarga tashakkurimizni ham ayta olmadik,
+                  qhilingan ishdan hamma mamnun bo‘ldi, qilgan ishinglar hammaga
+                  yoqdi, did bilan yondoshganilar bilinib turibti. Sizlar bilan
+                  hamkorlik qilganimizdan xursandmiz.
+                </p>
+                <div>
+                  <img src="/GroupiTGO.png" />
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-4">
+              <div className="bg_item">
+                <div>
+                  <img src="/PicQudratA.png" />
+                </div>
+                <h5>Qudrat Abdurahimov</h5>
+                <span>GITA Kompaniyasi Founderi</span>
+                <p>
+                  Xammaga katta rahmat yigitlar. Nasib qilsa yaxshi kunlarda
+                  ko‘rishamiz. Karantin tugasa ko‘rishib qolsak, qo‘llaringizni
+                  ushlab rahmat aytib qo‘yaman.
+                </p>
+                <div>
+                  <img src="/GroupGITA.png" />
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-4">
+              <div className="bg_item">
+                <div>
+                  <img src="/PicSunnah.png" />
+                </div>
+                <h5>Sobirjon Yaxshiyev</h5>
+                <span>Sunnah product asoschisi</span>
+                <p>
+                  Uzingiz va shogirdlarni extiyot qiling ko‘p kerak bolasizlar
+                  ko‘pchlika. O‘zbekistonda dizayin sohasi tanazzulga uchramasin
+                </p>
+                <div>
+                  <img src="/GroupSunnah_LOGO.png" />
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-4">
+              <div className="bg_item">
+                <div>
+                  <img src="/PicAbdurahmon_Nasriddin.png" />
+                </div>
+                <h5>Abdurahmon Nasriddinov</h5>
+                <span>Sardor brendi Founderi</span>
+                <p>
+                  Ishingizni mahsulini kurib xalovat qilyapsizmi? Qo‘llaring
+                  dard ko‘rmasin mexnatlaring halol
+                </p>
+                <div>
+                  <img src="/SardorSardor.jpg" />
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-4">
+              <div className="bg_item">
+                <div>
+                  <img src="/PicAhmadSaidov.png" />
+                </div>
+                <h5>Ahmad Saidov</h5>
+                <span>Greenmotors asoschisi</span>
+                <p>
+                  Qolgan dizaynlarham zo‘r chiqdi, qiyin bo‘ldi tanlash, ming
+                  bor uzur, ishlarilarga rivoj bersin, xamma qatnashganlarga
+                  raxmat, salomat bo‘lilar.
+                </p>
+                <div>
+                  <img src="/Слой x0020 1Green_MOTOROS.png" />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <header>
+        <div className="container">
+          <div className="row">
+            <div className="header_top col-lg-8">
+              <h2>
+                Nima uchun bizga ishonch bildirishadi? <span>5 ta </span>sabab
+              </h2>
+              <p>
+                RedFox dizayn studiyasi bir vaqtda ko’plab kompaniyalar bilan
+                ishlaydigan tajribali katta jamoadir. Mijozlarimiz qatorida
+                sizni ham ko’rishdan mamnunmiz.
+              </p>
+            </div>
+            <div className="header_bottom">
+              <div className="text_line">
+                <span>afzalliklar</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </header>
+      {/* Beliver || Ishonch */}
+      <div className="belive">
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-4">
+              <div>
+                <img src="/1one.jpg"></img>
+              </div>
+              <span>Tanlash imkoniyati ko'pligi</span>
+              <p>
+                RedFox sizning buyurtmangiz uchun 10 dan ortiq sara ishlar
+                jamlanmasini taqdim qiladi va siz ularning ichidan eng sarasini
+                tanlab olishingiz mumkin.
+              </p>
+            </div>
+            <div className="col-lg-4">
+              <div>
+                <img src="/2two.jpg"></img>
+              </div>
+              <span>24/7 rejimda muloqot mavjudligi</span>
+              <p>
+                Xalqaro miqyosda tan olingan va eng a'lo sifatda baholangan 24/7
+                menejer support buyurtmangiz yakunlanguncha sizning
+                xizmatingizda.
+              </p>
+            </div>
+            <div className="col-lg-4">
+              <div>
+                <img src="3three.jpg"></img>
+              </div>
+              <span>Sohalar qamrovi kengligi</span>
+              <p>
+                Studiyada dizaynning barcha sohalari bo'yicha mutaxassislar
+                borligi sizning barcha ishingiz bir joyda, shuningdek uyg‘un
+                holatda mukammal bitishini ta'minlaydi.
+              </p>
+            </div>
+            <div className="col-lg-4">
+              <div>
+                <img src="/4four.jpg"></img>
+              </div>
+              <span>Yirik loyihalardagi tajriba</span>
+              <p>
+                Bugungi kunga qadar studiyamiz 1000 dan ortiq muvaffaqiyatli ish
+                topshirgan. Hamda ko'plab o'rta biznes egalari va (Enterprise)
+                katta korxonalar bilan amalga oshirilgan loyihalar.
+              </p>
+            </div>
+            <div className="col-lg-4">
+              <div>
+                <img src="/5five.jpg"></img>
+              </div>
+              <span>Eng muhimi</span>
+              <p>
+                Biz o'z kasbimizni sevamiz va barchasini chin dildan bajaramiz.
+                Bu ham albatta sizning foydangizga. Qolganini bizning Portfolio
+                (https://t.me/portfolius) gapirsin.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <header>
+        <div className="container">
+          <div className="row">
+            <div className="header_top col-lg-8">
+              <h2>
+                Istalgan vaqt buyurtma bering: <span>97 778 5008 </span>
+              </h2>
+              <p>
+                Bizdan mijozlarimiz, tezkor xizmat, sifatli ishlar, ajoyib
+                muomila va shu kabi ko’plab holatlardan hursand bo’lishadi hamda
+                o’z fikrlarini bildirishadi.
+              </p>
+            </div>
+            <div className="write col-lg-12">
+              <h3>Online yozing</h3>
+            </div>
+          </div>
+        </div>
+      </header>
     </MainLayout>
   );
-}4
+}
