54df42592ec1ffefd580a51f879539b7b61eb797
yusuf <yusufsaydullayev@gmail.com>
2021-07-01 11:18:58 +0500

    router

--- pages/portfolio/[id].js
+++ pages/portfolio/[id].js
@@ -1,11 +1,10 @@
-import MainLayout from '../../components/MainLayout.js';
-import router from 'next/router';
+import MainLayout from "../../components/MainLayout.js";
+// import router from "next/router";
 
 export default function Id() {
- 
   return (
     <MainLayout>
-      <h1>{router.query.id}</h1>
+      <h1>ID</h1>
     </MainLayout>
-  )
+  );
 }
