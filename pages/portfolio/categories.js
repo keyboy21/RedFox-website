acf3fe86ff81a7f94f914e83b4569eedaa226ee4
yusuf591-cpu <59925798+yusuf591-cpu@users.noreply.github.com>
2021-07-17 14:18:05 +0500

    getStaticProps

--- pages/portfolio/categories.js
+++ pages/portfolio/categories.js
@@ -129,7 +129,7 @@ export default function Categories({ data }) {
   );
 }
 
-export async function getServerSideProps() {
+export async function getStaticProps() {
   const res = await fetch(`https://redfox.frilansus.com/api/apiportfolio`);
   const data = await res.json();
   return { props: { data } };
