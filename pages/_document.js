15e9c662ef09bbca7c50933e8f3896eb757576e5
yusuf591-cpu <59925798+yusuf591-cpu@users.noreply.github.com>
2021-07-20 18:09:37 +0500

    lazy

--- pages/_document.js
+++ pages/_document.js
@@ -16,7 +16,7 @@ export default class MyDocument extends Document {
           <script
             src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js"
             integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4"
-            crossOrigin="anonymous"
+            crossOrigin="anonymous" load="lazy"
           />
         </body>
       </Html>
