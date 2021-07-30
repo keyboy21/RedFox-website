88455b70dd61d0b675468b98e6ddf9722fad3df2
yusuf591-cpu <59925798+yusuf591-cpu@users.noreply.github.com>
2021-07-30 18:20:28 +0500

    changed all

--- pages/portfolio/categories.js
+++ pages/portfolio/categories.js
@@ -1,136 +1,57 @@
-import Index from "./index.js";
+import { useMemo, useState } from "react";
+import Portfolio from "./index.js";
 import Link from "next/link";
+import Image from 'next/image'
 
-export default function Categories({ data }) {
-  const firsFilter = data.filter((item) => {
-    return item.category_id == 1;
-  });
-  const secondFilter = data.filter((item) => {
-    return item.category_id == 2;
-  });
-  const threeFilter = data.filter((item) => {
-    return item.category_id == 3;
-  });
-  const fourFilter = data.filter((item) => {
-    return item.category_id == 4;
-  });
+const Categories = ({ data, category }) => {
+  const [local, setLocal] = useState(data)
 
   return (
-    <>
-      <Index>
-        <div className="categories">
-          <div className="prime">
-            <h3>
-              Branding <span>({firsFilter.length})</span>
-            </h3>
-            <div className="row hide">
-              {firsFilter.map((brand) => {
-                return (
-                  <Link href={`/portfolio/${brand.id}`} >
-                    <div className="col-lg-4 col-md-4 col-sm-6 col-12">
-                      <img
-                        rel="preload"
-                        as="image"
-                        src={`https://redfox.frilansus.com/frontend/images/${brand.img.slice(
-                          8
-                        )}`}
-                        alt="work."
-                        decoding="auto"
-                      />
-                      <p>{brand.title_uz}</p>
-                    </div>
-                  </Link>
-                );
-              })}
+    <Portfolio>
+      <div className="categories">
+        {category.map((name) => {
+          const firsFilter = useMemo(() => {
+            return local.filter((item) => item.category_id == name.id);
+          }, [local])
+          return (
+            <div className="prime" key={name.id}>
+              <h3>
+                {name.title_uz} <span>({firsFilter.length})</span>
+              </h3>
+              <div className="row hide">
+                {firsFilter.map((works) => {
+                  return (
+                    <Link href={`/portfolio/${works.id}`} key={works.id}>
+                      <div className="col-lg-4 col-md-4 col-sm-6 col-12">
+                        <Image
+                          width={411}
+                          height={320}
+                          style={{ cursor: "pointer" }}
+                          preload="true"
+                          src={`https://redfox.frilansus.com/${works.logo}`}
+                          alt="work."
+                        />
+                        <p>{works.title_uz}</p>
+                      </div>
+                    </Link>
+                  )
+                })}
+              </div>
             </div>
-          </div>
-
-          <div className="prime">
-            <h3>
-              Naming <span>({secondFilter.length})</span>
-            </h3>
-            <div className="row hide">
-              {secondFilter.map((brand) => {
-                return (
-                  <Link href={`/portfolio/${brand.id}`} key={brand.id}>
-                    <div className="col-lg-4 col-md-4 col-sm-6">
-                      <img
-                        rel="preload"
-                        as="image"
-                        src={`https://redfox.frilansus.com/frontend/images/${brand.img.slice(
-                          8
-                        )}`}
-                        alt="work."
-                        decoding="auto"
-                      />
-                      <p>{brand.title_uz}</p>
-                    </div>
-                  </Link>
-                );
-              })}
-            </div>
-          </div>
-
-          <div className="prime">
-            <h3>
-              Web & Mobile <span>({threeFilter.length})</span>
-            </h3>
-            <div className="row hide">
-              {threeFilter.map((brand) => {
-                return (
-                  <Link href={`/portfolio/${brand.id}`} key={brand.id}>
-                    <div className="col-lg-4 col-md-4 col-sm-6">
-                      <img
-                        rel="preload"
-                        as="image"
-                        src={`https://redfox.frilansus.com/frontend/images/${brand.img.slice(
-                          8
-                        )}`}
-                        alt="work."
-                        decoding="auto"
-                      />
-                      <p>{brand.title_uz}</p>
-                    </div>
-                  </Link>
-                );
-              })}
-            </div>
-          </div>
-
-          <div className="prime">
-            <h3>
-              Packaging <span>({fourFilter.length})</span>
-            </h3>
-            <div className="row hide">
-              {fourFilter.map((brand) => {
-                return (
-                  <Link href={`/portfolio/${brand.id}`} key={brand.id}>
-                    <div className="col-lg-4 col-md-4 col-sm-6">
-                      <img
-                        rel="preload"
-                        as="image"
-                        src={`https://redfox.frilansus.com/frontend/images/${brand.img.slice(
-                          8
-                        )}`}
-                        alt="work."
-                        decoding="auto"
-                      />
-                      <p>{brand.title_uz}</p>
-                    </div>
-                  </Link>
-                );
-              })}
-            </div>
-          </div>
-        </div>
-        <button className="btn_portfolio">Shu yerga bosing</button>
-      </Index>
-    </>
+          )
+        })}
+      </div>
+      {/* <button className="btn_portfolio">Shu yerga bosing</button> */}
+    </Portfolio>
   );
 }
+export default Categories;
 
 export async function getStaticProps() {
-  const res = await fetch(`https://redfox.frilansus.com/api/apiportfolio`);
+  const res = await fetch(`https://redfox.frilansus.com/api/portfolio/`);
   const data = await res.json();
-  return { props: { data } };
+  const cat = await fetch(`https://redfox.frilansus.com/api/category/`);
+  const category = await cat.json();
+  return { props: { data, category } };
 }
+
