682a8158ac76efdf2c16ced02cccb0302c1d57ae
yusuf591-cpu <59925798+yusuf591-cpu@users.noreply.github.com>
2021-07-30 18:22:39 +0500

    changed all

--- pages/portfolio/all.js
+++ pages/portfolio/all.js
@@ -1,39 +1,41 @@
-import Index from "./index.js";
+import Portfolio from "./index.js";
 import Link from "next/link";
+import Image from 'next/image';
 
 const All = ({ data }) => {
   return (
-    <>
-      <Index>
-        <div className="all">
-          <div className="row">
-            {data.map((all) => {
-              return (
-                <Link href={`/portfolio/${all.id}`} key={all.id}>
-                  <div className="col-lg-4 col-md-4 col-sm-6">
-                    <img
-                      rel="preload"
-                      as="image"
-                      src={`https://redfox.frilansus.com/${all.logo}`}
-                      alt="work."
-                      decoding="auto"
-                    />
-                    <p>{all.title_uz}</p>
-                  </div>
-                </Link>
-              );
-            })}
-          </div>
+    <Portfolio>
+      <div className="all">
+        <div className="row">
+          {data.map((all) => {
+            return (
+              <Link href={`/portfolio/${all.id}`} key={all.id}>
+                <div className="col-lg-4 col-md-4 col-sm-6">
+                  <Image
+                    width={546}
+                    height={431}
+                    style={{ cursor: "pointer" }}
+                    preload="true"
+                    src={`https://redfox.frilansus.com/${all.logo}`}
+                    alt="work."
+                  />
+                  <p>{all.title_uz}</p>
+                </div>
+              </Link>
+            );
+          })}
         </div>
-        <button className="btn_portfolio">Shu yerga bosing</button>
-      </Index>
-    </>
+      </div>
+      {/* <button className="btn_portfolio">Shu yerga bosing</button> */}
+    </Portfolio>
+
   );
 };
 
 export async function getStaticProps() {
-  const res = await fetch(`https://redfox.frilansus.com/api/apiportfolio`);
+  const res = await fetch(`https://redfox.frilansus.com/api/portfolio`);
   const data = await res.json();
   return { props: { data } };
 }
 export default All;
+
