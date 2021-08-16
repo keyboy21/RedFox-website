151d815763f6a3aed0109f48683045b594e6df34
yusuf591-cpu <yusufsaydullayev@gmail.com>
2021-08-16 13:52:57 +0500

    css

--- pages/_document.js
+++ pages/_document.js
@@ -13,7 +13,6 @@ export default class MyDocument extends Document {
             integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4"
             crossOrigin="anonymous" loading="lazy"
           />
-
         </body>
       </Html>
     );
