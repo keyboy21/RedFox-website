3110c125c2559d0026d80a87cee2c0afa7620285
yusuf <yusufsaydullayev@gmail.com>
2021-08-22 13:33:17 +0500

    bootstrap js

--- pages/_app.js
+++ pages/_app.js
@@ -28,6 +28,9 @@ function MyApp({ Component, pageProps }) {
       });
     }
   }, [])
+  useEffect(() => {
+    typeof document !== undefined ? require('bootstrap/dist/js/bootstrap.min.js') : null
+  }, [])
   return (
     <>
       <NextNprogress
--- pages/_document.js
+++ pages/_document.js
@@ -8,11 +8,11 @@ export default class MyDocument extends Document {
         <body>
           <Main />
           <NextScript />
-          <script
+          {/* <script
             src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js"
             integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4"
             crossOrigin="anonymous" loading="lazy"
-          />
+          /> */}
         </body>
       </Html>
     );
